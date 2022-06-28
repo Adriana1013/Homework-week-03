312ec4163cec2c2737773dde0eb8efb15b9a278e
Adriana Villegas <villegas.a102@gmail.com>
2022-06-27 23:12:46 -0500

    prompt & confirm

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
 
 function generatePassword(){
-  window.prompt("Enter a password length between 8 to 128 characters")
+  var passwordLength = window.prompt("Enter a password length")
+  console.log(passwordLength)
+  var lowercaseLetters = window.confirm ("Would you like to include lowercase letters?")
+  console.log(lowercaseLetters)
+  var uppercaseLetters = window.confirm("Would you like to include uppercase letters?")
+  console.log(uppercaseLetters)
+  var numericSymbols = window.confirm("Would you like to include numeric symbols?")
+  console.log(numericSymbols)
+  var specialCharacters = window.confirm("Would you like to include special characters?")
+  console.log(specialCharacters)
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+  
